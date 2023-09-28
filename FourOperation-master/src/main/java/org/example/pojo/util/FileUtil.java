9f3f0672f6546f0b72eb926597e35789c86cd908
stopyc <e13242597082@outlook.com>
2023-09-28 11:09:16 +0800

    feat：完成文件工具类

---
+++ FourOperation-master/src/main/java/org/example/pojo/util/FileUtil.java
@@ -0,0 +1,71 @@
+package org.example.pojo.util;
+
+import org.example.pojo.exception.OperandException;
+import org.example.pojo.po.Equation;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 文件工具类
+ * @author 3121005035, OuroborosNo2
+ * */
+public class FileUtil {
+    /**
+     * 将内容写入指定路径文件
+     * @param path 文件路径
+     * @param context 写入内容
+     * @return 成功与否
+     * */
+    public static boolean write(String path, List<String> context){
+        try {
+            List<String> list = new ArrayList<>();
+            //加上序号
+            for(int i=0;i<context.size();i++) {
+                list.add(i, i + "、" + context.get(i));
+            }
+            Files.write(Paths.get(path), list);
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    /**
+     * 从指定路径文件读取成表达式数组
+     * @param path 文件路径
+     * @return 表达式数组
+     * */
+    public static List<Equation> readAsEquation(String path){
+        List<Equation> lines;
+        try {
+            lines = Files.readAllLines(Paths.get(path)).stream().map(s -> {
+                try {
+                    //除去序号
+                    return Equation.parse(s.substring(s.indexOf("、")+1));
+                } catch (OperandException e) {
+                    throw new RuntimeException(e);
+                }
+            }).toList();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return lines;
+    }
+    public static List<Float> readAsAnswer(String path){
+        List<Float> lines;
+        try {
+            //除去序号
+            lines = Files.readAllLines(Paths.get(path)).stream().map(s -> Float.parseFloat(s.substring(s.indexOf("、")+1))).toList();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return lines;
+    }
+}
